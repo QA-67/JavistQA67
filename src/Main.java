5047ae598d61738bfd2b2beb1fee232b8b91e6a5
Alexander Korovka QA67 <eedc2011ru@gmail.com>
2023-07-14 17:45:28 +0300

    add README

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Привет, Нетология!");
+        System.out.println("2+3");
+        int quantity = 50;
+        int x = quantity + 1;
+
+        double w = 50.5;
+
+        String number = "110";
+        System.out.println(number + " номер дачного дома " + w + " шагов до пруда ");
+    }
+}
